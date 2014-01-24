366885a76ac551e9d77e450d5569accfe8fa047a
Bartosz Pasinski <b.pasinski@neurosys.pl>
2014-01-24 09:16:10 +0100

    Changed Twig renderer

--- Renderer/TwigRenderer.php
+++ Renderer/TwigRenderer.php
@@ -3,7 +3,19 @@ namespace NeuroSYS\DoctrineDatatables\Renderer;
 
 use NeuroSYS\DoctrineDatatables\RendererInterface;
 
-class TwigRenderer extends \Twig_Environment implements RendererInterface
+class TwigRenderer implements RendererInterface
 {
+    private $environment;
+
+    public function __construct(\Twig_Environment $env)
+    {
+        $this->environment = $env;
+    }
+
+    public function render($template, array $values = array())
+    {
+        return $this->environment->render($template, $values);
+    }
+
 
 }
