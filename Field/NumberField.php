5f97f3d12b710db129e86b52a1d184caf7a8729a
Nicolas Sauveur <nicolas.sauveur@gmail.com>
2014-07-19 01:10:21 +0200

    add equality filter for number field - with fallback to range filter

--- Field/NumberField.php
+++ Field/NumberField.php
@@ -1,7 +1,27 @@
 <?php
 namespace NeuroSYS\DoctrineDatatables\Field;
 
+use Doctrine\ORM\QueryBuilder;
+
 class NumberField extends RangeField
 {
+    public function filter(QueryBuilder $qb)
+    {
+        if (strpos($this->getSearch(),',') === false) {
+            $search = $this->getSearch();
+            $expr = $qb->expr()->andX();
+            list ($searchField,) = $this->getSearchFields();
+            if ($search) {
+                $expr->add(
+                    $qb->expr()->eq($searchField, ':from_'.$this->getIndex())
+                );
+                $qb->setParameter('from_'.$this->getIndex(), $search);
+            }
+        } else {
+            $expr = parent::filter($qb);
+        }
+
+        return $expr;
+    }
 
 }
